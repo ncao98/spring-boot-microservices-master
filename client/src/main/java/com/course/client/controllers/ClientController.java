225606ed23b3b94752eda42b68f40575a3ba5f77
Nicolas CAO <n.cao@net.estia.fr>
2021-01-27 10:04:24 +0100

    Affichage vue détaillée

--- client/src/main/java/com/course/client/controllers/ClientController.java
+++ client/src/main/java/com/course/client/controllers/ClientController.java
@@ -3,11 +3,15 @@ package com.course.client.controllers;
 import com.course.client.beans.ProductBean;
 import com.course.client.proxies.MsProductProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ClientController {
@@ -23,5 +27,15 @@ public class ClientController {
         model.addAttribute("products", products);
 
         return "index";
+    }
+
+    @RequestMapping("/product-detail/{id}")
+    public String get(@PathVariable Long id) {
+        Optional<ProductBean> productInstance = msProductProxy.get(id);
+
+        if (!productInstance.isPresent())
+            throw new ResponseStatusException(HttpStatus.CREATED, "Specified product doesn't exist");
+
+        return "detail";
     }
 }
