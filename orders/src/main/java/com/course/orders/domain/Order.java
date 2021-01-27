40aa6c42904c83f480e009005f44d4b0f1239bfd
InesK6 <ines.kergale@yahoo.fr>
2021-01-27 11:44:02 +0100

    repository + domain

--- orders/src/main/java/com/course/orders/domain/Order.java
+++ orders/src/main/java/com/course/orders/domain/Order.java
@@ -1,4 +1,42 @@
 package com.course.orders.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long paidTotal;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", paidTotal=" + paidTotal +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPaidTotal() {
+        return paidTotal;
+    }
+
+    public void setPaidTotal(Long paidTotal) {
+        this.paidTotal = paidTotal;
+    }
+
+    public Order(Long id, Long paidTotal) {
+        this.id = id;
+        this.paidTotal = paidTotal;
+    }
 }
--- orders/src/main/java/com/course/orders/repositories/OrderRepository.java
+++ orders/src/main/java/com/course/orders/repositories/OrderRepository.java
@@ -1,4 +1,6 @@
 package com.course.orders.repositories;
 
-public interface OrderRepository {
+import com.course.orders.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;@
+public interface OrderRepository extends JpaRepository<Order,Long> {
 }
