d83215bbc9f3a6db3517fcc3f54525ebbad3295e
Nicolas CAO <n.cao@net.estia.fr>
2021-01-27 09:21:52 +0100

    Initial commit

---
+++ client/src/main/java/com/course/client/controllers/ClientController.java
@@ -0,0 +1,27 @@
+package com.course.client.controllers;
+
+import com.course.client.beans.ProductBean;
+import com.course.client.proxies.MsProductProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class ClientController {
+
+    @Autowired
+    private MsProductProxy msProductProxy;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+
+        List<ProductBean> products =  msProductProxy.list();
+
+        model.addAttribute("products", products);
+
+        return "index";
+    }
+}
